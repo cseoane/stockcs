a44ae9220cf3abd5b107ee4814b2a8bad0da4833
cseoane <cristian.j.seoane@gmail.com>
2020-07-11 21:55:17 -0300

    CRUD example v1

--- stockcs/stockapp/views.py
+++ stockcs/stockapp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
+from stockapp.models import Name
+from stockapp.forms import NameForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    names_from_db = Name.objects.all()
 
+    form = NameForm()
+
+    context_dict = {'names_from_context': names_from_db, 'form': form}
+
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, 'index.html', context_dict)
+        else:
+            print(form.errors)
+
+    return render(request, 'index.html', context_dict)
